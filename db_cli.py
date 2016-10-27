e4b5779ebdf461ced15ea88b5e15a77468de5f20
Rudy <rudy@Saintmood.local>
2016-10-28 00:02:36 +0300

    added basic create table generic method

--- db_cli.py
+++ db_cli.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
-
 import click
 import psycopg2
 
 from faker import Factory
+from sqlalchemy import (
+    Table,
+    MetaData,
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+    create_engine
+)
 
 import config
 
@@ -19,6 +27,15 @@ def _init_db_connection_pool():
         port=config.DB['POSTGRES_STORE']['PORT'],
         password=config.DB['POSTGRES_STORE']['PASSWORD']
     ))
+
+
+def _create_generic_table(table_name, column_count, metadata=None):
+    if metadata is None:
+        metadata = MetaData()
+    columnset = [
+        Column('id', Integer, primary_key=True)
+    ]
+    Table(table_name, metadata, *columnset)
 
 
 def _init_db_connection():
@@ -42,8 +59,18 @@ def generate_csv():
 
 
 @cli.command()
-def prepare_db():
+@click.argument('db', help='Set the DB you are going to work with.')
+@click.option('--tables-count', type=click.INT, help='Specify how many tables you want to have in DB.')
+@click.option('--table-width', type=click.INT, help='Specify how many columns every table should has.')
+def prepare_db(db, tables_count, table_width ):
     click.echo('InitDB is executing')
+    metadata = MetaData()
+
+    for table_index in range(tables_count):
+        _create_generic_table('some_name', table_width, metadata=metadata)
+
+    engine = create_engine("postgresql://scott:tiger@localhost/test")
+    metadata.create_all(engine)
 
 
 if __name__ == '__main__':
