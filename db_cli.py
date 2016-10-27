6441a7f67c82e27b0c418fc1dfa37542dbe52a0c
lpfan <misha.shchetinin@gmail.com>
2016-10-27 17:57:28 +0300

    extend db_cli with db_connection factories

--- db_cli.py
+++ db_cli.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 import click
+import psycopg2
 
 from faker import Factory
-from psycopg2.pool import ThreadedConnectionPool
 
 import config
 
@@ -11,8 +11,18 @@ import config
 db_config_str = "dbname='{db}' user='{user}' host='{host}' port='{port}' password='{password}'"
 
 
-def init_connection_pool():
-    return ThreadedConnectionPool(1, 100, db_config_str.format(
+def _init_db_connection_pool():
+    return psycopg2.pool.ThreadedConnectionPool(1, 100, db_config_str.format(
+        db=config.DB['POSTGRES_STORE']['DB'],
+        user=config.DB['POSTGRES_STORE']['USER'],
+        host=config.DB['POSTGRES_STORE']['HOST'],
+        port=config.DB['POSTGRES_STORE']['PORT'],
+        password=config.DB['POSTGRES_STORE']['PASSWORD']
+    ))
+
+
+def _init_db_connection():
+    return psycopg2.connect(db_config_str.format(
         db=config.DB['POSTGRES_STORE']['DB'],
         user=config.DB['POSTGRES_STORE']['USER'],
         host=config.DB['POSTGRES_STORE']['HOST'],
